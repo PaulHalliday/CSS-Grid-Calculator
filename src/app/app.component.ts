af878174a0d220b3413b60c3e7e1594fe0271b9a
Paul Halliday <paulhalliday@pauls-imac.home>
2017-10-19 02:56:42 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
+
+  calc = [
+    { name: 'C', operator: true },
+    { name: '()', operator: true },
+    { name: '%', operator: true },
+    { name: '/', operator: true },
+    { name: '7', operator: false },
+    { name: '8', operator: false },
+    { name: '9', operator: false },
+    { name: '*', operator: true },
+    { name: '4', operator: false },
+    { name: '5', operator: false },
+    { name: '6', operator: false },
+    { name: '-', operator: true },
+    { name: '1', operator: false },
+    { name: '2', operator: false },
+    { name: '3', operator: false },
+    { name: '+', operator: true },
+    { name: '0', operator: false },
+    { name: '.', operator: true },
+    { name: '<', operator: true },
+    { name: '=', operator: true },
+  ];
 }
